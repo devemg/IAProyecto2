5df901f0feb5e23fa1b75a7add8a81c30321aec9
devemg <garciam.emm@gmail.com>
2021-05-02 14:58:44 -0600

    Posicion Planta
    Posiciones de planta

--- Assets/Scripts/CebraPlanta.cs
+++ Assets/Scripts/CebraPlanta.cs
@@ -8,27 +8,27 @@ public class CebraPlanta : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        setNO();   
+        center();   
     }
 
     void setNE() {
-        gameObject.transform.Translate(1.3f, 0, 0.95f);
+        gameObject.transform.Translate(-2.6f, 0, 2f);
     }
 
     void setNO(){
-        gameObject.transform.Translate(-1.3f, 0, -0.95f);
+        gameObject.transform.Translate(0f, 0, 2f);
     }
 
     void setSE(){
-        gameObject.transform.Translate(1.3f, 0, 1.3f);
+        gameObject.transform.Translate(-2.6f, 0, -0.6f);
     }
 
     void setSO(){
-        gameObject.transform.Translate(-1.3f, 0, 1.3f);
+        gameObject.transform.Translate(-0.2f, 0, -0.6f);
     }
 
     void center(){
-        gameObject.transform.Translate(0, 0, 0);
+        gameObject.transform.Translate(-1.3f, 0, 0.5f);
     }
 
     // Update is called once per frame
