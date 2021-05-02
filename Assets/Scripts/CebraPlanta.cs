d366659a43956d1f0afa6240ab966bb11db07187
devemg <garciam.emm@gmail.com>
2021-05-02 16:49:43 -0600

    Posiciones 4/5
    Objetos posicionados 4 de 5 correctos

--- Assets/Scripts/CebraPlanta.cs
+++ Assets/Scripts/CebraPlanta.cs
@@ -8,7 +8,7 @@ public class CebraPlanta : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        center();   
+        setNE(); 
     }
 
     void center(){
