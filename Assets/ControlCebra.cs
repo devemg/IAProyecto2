9e86fa40623b4404ca15c70c1bbc2b4ba51ad6aa
devemg <garciam.emm@gmail.com>
2021-05-02 18:26:53 -0600

    gettets en Archivo de control

--- Assets/ControlCebra.cs
+++ Assets/ControlCebra.cs
@@ -7,24 +7,12 @@ public class ControlCebra : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameObject planta = GameObject.Find("Cebra_planta");
-        CebraPlanta other = (CebraPlanta) planta.GetComponent(typeof(CebraPlanta));
-        other.setSE();
-
-        GameObject sillon = GameObject.Find("Cebra_sillon");
-        CebraSofa other1 = (CebraSofa) sillon.GetComponent(typeof(CebraSofa));
-        other1.setNE();
-
         
+        GetPlanta().setSE();
         GetMesa().center();
-
-        GameObject silla = GameObject.Find("Cebra_silla");
-        CebraSilla other3 = (CebraSilla) silla.GetComponent(typeof(CebraSilla));
-        other3.setSO();
-
-        GameObject lamp = GameObject.Find("Cebra_lampara");
-        CebraLampara other4 = (CebraLampara) lamp.GetComponent(typeof(CebraLampara));
-        other4.setNO();
+        GetSilla().setSO();
+        GetLampara().setNO();
+        GetSofa().setNE();
     }
 
     // Update is called once per frame
@@ -38,4 +26,24 @@ public class ControlCebra : MonoBehaviour
         return (CebraMesa) mesa.GetComponent(typeof(CebraMesa));
     }
 
+    public CebraSilla GetSilla() {
+        GameObject silla = GameObject.Find("Cebra_silla");
+        return (CebraSilla) silla.GetComponent(typeof(CebraSilla));
+    }
+
+    public CebraLampara GetLampara(){
+        GameObject lamp = GameObject.Find("Cebra_lampara");
+        return (CebraLampara) lamp.GetComponent(typeof(CebraLampara));
+    }
+
+    public CebraSofa GetSofa(){
+        GameObject sillon = GameObject.Find("Cebra_sillon");
+        return (CebraSofa) sillon.GetComponent(typeof(CebraSofa));
+        
+    }
+
+    public CebraPlanta GetPlanta() {
+        GameObject planta = GameObject.Find("Cebra_planta");
+        return (CebraPlanta) planta.GetComponent(typeof(CebraPlanta));
+    }
 }
